9664bea5e1b05fb324d87d5af6e33d3d2136fc4c
olaide <olaideadeleke25@gmail.com>
2023-07-23 14:23:52 -0700

    new

--- print_c.c
+++ print_c.c
@@ -11,39 +11,39 @@
 
 int _printf(const char *format, ...)
 {
-  va_list a;
-  int i;
-  int len = 0;
-  char *e;
-  
-  va_start(a, format);
-  for (i = 0; format[i] != '\0'; i++)
-    {
-      if (format[i] == '%')
-      {
-        i++;
-        if (format[i] == 'c')
-        {
-          e = va_arg(a, char *);
-          len += print_a(*e);
-        }
-        else if (format[i] == 's')
-        {
-          e = va_arg(a, char *);
-          len += print_b(e);
-        }
-        else 
-        {
-          len++;
-        }
-      }
-        else
-      {
-        len++;
-      }
-    }
-  va_end(a);
-  return (len);
+	va_list a;
+	int i;
+	int len = 0;
+	char *e;
+
+	va_start(a, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				e = va_arg(a, char *);
+				len += print_a(*e);
+			}
+			else if (format[i] == 's')
+			{
+				e = va_arg(a, char *);
+				len += print_b(e);
+			}
+			else
+			{
+				len++;
+			}
+		}
+		else
+		{
+			len++;
+		}
+	}
+	va_end(a);
+	return (len);
 }
 
 /**
@@ -53,7 +53,7 @@ int _printf(const char *format, ...)
  */
 char print_a(char a)
 {
-  return (a);
+	return (a);
 }
 
 /**
@@ -63,21 +63,19 @@ char print_a(char a)
  */
 char *print_b(char *b)
 {
-  int lent = strlen(b);
-  int i;
-  char *c;
+	int lent = strlen(b);
+	int i;
+	char *c;
 
-  if (b == NULL)
-    return (NULL);
-  
-  c = malloc(lent * sizeof(char));
-  if (c == NULL)
-    return (NULL);
-
-  for (i = 0; i < lent; i++)
-    {
-      c[i] = b[i];
-    }
-  free(c);
-  return (c);
+	if (b == NULL)
+		return (NULL);
+	c = malloc(lent * sizeof(char));
+	if (c == NULL)
+		return (NULL);
+	for (i = 0; i < lent; i++)
+	{
+		c[i] = b[i];
+	}
+	free(c);
+	return (c);
 }
