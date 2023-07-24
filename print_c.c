c77c48ddec74a20e938393384bbbbf012a333094
Replit user <>
2023-07-24 20:59:19 +0000

    new test

--- print_c.c
+++ print_c.c
@@ -11,46 +11,45 @@
 int _printf(const char *format, ...)
 {
 	va_list a;
-	int i, k = 0, len = 0;
-	char g, *j, *e;
+	int i, len = 0;
+	char g, *e;
 
 	va_start(a, format);
 	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == 'c')
-			{
-				g = va_arg(a, int);
-				_putchar(g);
-				len++;
-			}
-			else if (format[i] == 's')
-			{
-				e = va_arg(a, char *);
-				if (e != NULL)
-				{
-					for (; *e; e++)
-					{
-						_putchar(*e);
-						len++;
-					}
-				}
-			}
-		}
-		else
-		{
-      j = va_arg(a, char *);
-      if (j != NULL)
+    {
+      if (format[i] == '%')
+      {
+        i++;
+        if (format[i] == '%')
         {
-          while (j[k] < '\0')
-          _putchar(j[k]);
-          k++;
+          _putchar('%');
           len++;
         }
-		}
-	}
-	va_end(a);
-	return (len);
+        else if (format[i] == 'c')
+        {
+          g = va_arg(a, int);
+          _putchar(g);
+          len++;
+        }
+        else if (format[i] == 's')
+        {
+          e = va_arg(a, char *);
+          if (e != NULL)
+          {
+            for (; *e; e++)
+              {
+                _putchar(*e);
+                len++;
+              }
+          }
+        }
+      }
+      else
+      {
+        _putchar(format[i]);
+        len++;
+      }
+    }
+  va_end(a);
+  return (len);
 }
